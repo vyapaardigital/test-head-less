afed3ab549a8f37e0e4209f5c248ed174d8c5fc0
vyapaardigital <vyapaardigital@gmail.com>
2025-02-02 01:08:15 +0530

    Update single.tsx

--- src/wp-templates/single.tsx
+++ src/wp-templates/single.tsx
@@ -1,247 +1,258 @@
-import { gql } from "../__generated__";
+import { gql } from '../__generated__'
 import {
-  GetPostSiglePageQuery,
-  NcgeneralSettingsFieldsFragmentFragment,
-  NcmazFcUserReactionPostActionEnum,
-  NcmazFcUserReactionPostNumberUpdateEnum,
-} from "../__generated__/graphql";
-import { FaustTemplate } from "@faustwp/core";
-import SingleContent from "@/container/singles/SingleContent";
-import SingleType1 from "@/container/singles/single/single";
-import { getPostDataFromPostFragment } from "@/utils/getPostDataFromPostFragment";
-import { Sidebar } from "@/container/singles/Sidebar";
-import PageLayout from "@/container/PageLayout";
-import { FOOTER_LOCATION, PRIMARY_LOCATION } from "@/contains/menu";
-import dynamic from "next/dynamic";
-import { useEffect, useState } from "react";
-import { NC_MUTATION_UPDATE_USER_REACTION_POST_COUNT } from "@/fragments/mutations";
-import { useMutation } from "@apollo/client";
-import { IS_DEV } from "@/contains/site-settings";
-import { useSelector } from "react-redux";
-import { RootState } from "@/stores/store";
-import useGetPostsNcmazMetaByIds from "@/hooks/useGetPostsNcmazMetaByIds";
-import { TPostCard } from "@/components/Card2/Card2";
-import { useRouter } from "next/router";
-import { TCategoryCardFull } from "@/components/CardCategory1/CardCategory1";
-import SingleTypeAudio from "@/container/singles/single-audio/single-audio";
-import SingleTypeVideo from "@/container/singles/single-video/single-video";
-import SingleTypeGallery from "@/container/singles/single-gallery/single-gallery";
+	GetPostSiglePageQuery,
+	NcgeneralSettingsFieldsFragmentFragment,
+	NcmazFcUserReactionPostActionEnum,
+	NcmazFcUserReactionPostNumberUpdateEnum,
+} from '../__generated__/graphql'
+import { FaustTemplate } from '@faustwp/core'
+import SingleContent from '@/container/singles/SingleContent'
+import SingleType1 from '@/container/singles/single/single'
+import { getPostDataFromPostFragment } from '@/utils/getPostDataFromPostFragment'
+<!-- AddToAny BEGIN -->
+<div class="a2a_kit a2a_kit_size_32 a2a_default_style">
+<a class="a2a_dd" href="https://www.addtoany.com/share"></a>
+<a class="a2a_button_whatsapp"></a>
+<a class="a2a_button_facebook"></a>
+<a class="a2a_button_telegram"></a>
+<a class="a2a_button_x"></a>
+</div>
+<script defer src="https://static.addtoany.com/menu/page.js"></script>
+<!-- AddToAny END -->
+import { Sidebar } from '@/container/singles/Sidebar'
+import PageLayout from '@/container/PageLayout'
+import { FOOTER_LOCATION, PRIMARY_LOCATION } from '@/contains/menu'
+import dynamic from 'next/dynamic'
+import { useEffect, useState } from 'react'
+import { NC_MUTATION_UPDATE_USER_REACTION_POST_COUNT } from '@/fragments/mutations'
+import { useMutation } from '@apollo/client'
+import { useSelector } from 'react-redux'
+import { RootState } from '@/stores/store'
+import useGetPostsNcmazMetaByIds from '@/hooks/useGetPostsNcmazMetaByIds'
+import { TPostCard } from '@/components/Card2/Card2'
+import { useRouter } from 'next/router'
+import { TCategoryCardFull } from '@/components/CardCategory1/CardCategory1'
+import SingleTypeAudio from '@/container/singles/single-audio/single-audio'
+import SingleTypeVideo from '@/container/singles/single-video/single-video'
+import SingleTypeGallery from '@/container/singles/single-gallery/single-gallery'
 
 const DynamicSingleRelatedPosts = dynamic(
-  () => import("@/container/singles/SingleRelatedPosts")
-);
+	() => import('@/container/singles/SingleRelatedPosts'),
+)
 const DynamicSingleType2 = dynamic(
-  () => import("../container/singles/single-2/single-2")
-);
+	() => import('../container/singles/single-2/single-2'),
+)
 const DynamicSingleType3 = dynamic(
-  () => import("../container/singles/single-3/single-3")
-);
+	() => import('../container/singles/single-3/single-3'),
+)
 const DynamicSingleType4 = dynamic(
-  () => import("../container/singles/single-4/single-4")
-);
+	() => import('../container/singles/single-4/single-4'),
+)
 const DynamicSingleType5 = dynamic(
-  () => import("../container/singles/single-5/single-5")
-);
+	() => import('../container/singles/single-5/single-5'),
+)
 
 const Component: FaustTemplate<GetPostSiglePageQuery> = (props) => {
-  //  LOADING ----------
-  if (props.loading) {
-    return <>Loading...</>;
-  }
+	//  LOADING ----------
+	if (props.loading) {
+		return <>Loading...</>
+	}
 
-  const router = useRouter();
-  const IS_PREVIEW = router.pathname === "/preview";
+	const router = useRouter()
+	const IS_PREVIEW = router.pathname === '/preview'
 
-  // START ----------
-  const { isReady, isAuthenticated } = useSelector(
-    (state: RootState) => state.viewer.authorizedUser
-  );
-  const { viewer } = useSelector((state: RootState) => state.viewer);
-  const [isUpdateViewCount, setIsUpdateViewCount] = useState(false);
+	// START ----------
+	const { isReady, isAuthenticated } = useSelector(
+		(state: RootState) => state.viewer.authorizedUser,
+	)
+	const { viewer } = useSelector((state: RootState) => state.viewer)
+	const [isUpdateViewCount, setIsUpdateViewCount] = useState(false)
 
-  useEffect(() => {
-    const timeOutUpdateViewCount = setTimeout(() => {
-      setIsUpdateViewCount(true);
-    }, 5000);
+	useEffect(() => {
+		const timeOutUpdateViewCount = setTimeout(() => {
+			setIsUpdateViewCount(true)
+		}, 5000)
 
-    return () => {
-      clearTimeout(timeOutUpdateViewCount);
-    };
-  }, []);
+		return () => {
+			clearTimeout(timeOutUpdateViewCount)
+		}
+	}, [])
 
-  const _post = props.data?.post || {};
-  const _relatedPosts = (props.data?.posts?.nodes as TPostCard[]) || [];
-  const _top10Categories =
-    (props.data?.categories?.nodes as TCategoryCardFull[]) || [];
+	const _post = props.data?.post || {}
 
-  const {
-    title,
-    ncPostMetaData,
-    postFormats,
-    featuredImage,
-    databaseId,
-    excerpt,
-  } = getPostDataFromPostFragment(_post);
+	// console.log('🚀 ~ file: single.tsx ~ line 68 ~ Component ~ _post', _post)
 
-  //
-  const {} = useGetPostsNcmazMetaByIds({
-    posts: (IS_PREVIEW ? [] : [_post]) as TPostCard[],
-  });
-  //
+	const _relatedPosts = (props.data?.posts?.nodes as TPostCard[]) || []
+	const _top10Categories =
+		(props.data?.categories?.nodes as TCategoryCardFull[]) || []
 
-  // Query update post view count
-  const [handleUpdateReactionCount, { reset }] = useMutation(
-    NC_MUTATION_UPDATE_USER_REACTION_POST_COUNT,
-    {
-      onCompleted: (data) => {
-        IS_DEV && console.log("___update post view data: ", data);
-        reset();
-      },
-    }
-  );
+	const {
+		title,
+		ncPostMetaData,
+		postFormats,
+		featuredImage,
+		databaseId,
+		excerpt,
+	} = getPostDataFromPostFragment(_post)
 
-  // update view count
-  useEffect(() => {
-    if (!isReady || IS_PREVIEW || !isUpdateViewCount) {
-      return;
-    }
+	//
+	const {} = useGetPostsNcmazMetaByIds({
+		posts: (IS_PREVIEW ? [] : [_post]) as TPostCard[],
+	})
+	//
 
-    // user chua dang nhap, va update view count voi user la null
-    if (isAuthenticated === false) {
-      handleUpdateReactionCount({
-        variables: {
-          post_id: databaseId,
-          reaction: NcmazFcUserReactionPostActionEnum.View,
-          number: NcmazFcUserReactionPostNumberUpdateEnum.Add_1,
-        },
-      });
-      return;
-    }
+	// Query update post view count
+	const [handleUpdateReactionCount, { reset }] = useMutation(
+		NC_MUTATION_UPDATE_USER_REACTION_POST_COUNT,
+		{
+			onCompleted: (data) => {
+				reset()
+			},
+		},
+	)
 
-    // user da dang nhap, va luc nay viewer dang fetch.
-    if (!viewer?.databaseId) {
-      return;
-    }
+	// update view count
+	useEffect(() => {
+		if (!isReady || IS_PREVIEW || !isUpdateViewCount) {
+			return
+		}
 
-    // khi viewer fetch xong, luc nay viewer da co databaseId, va se update view count voi user la viewer
-    handleUpdateReactionCount({
-      variables: {
-        post_id: databaseId,
-        reaction: NcmazFcUserReactionPostActionEnum.View,
-        number: NcmazFcUserReactionPostNumberUpdateEnum.Add_1,
-        user_id: viewer?.databaseId,
-      },
-    });
-  }, [
-    databaseId,
-    isReady,
-    isAuthenticated,
-    viewer?.databaseId,
-    IS_PREVIEW,
-    isUpdateViewCount,
-  ]);
+		// user chua dang nhap, va update view count voi user la null
+		if (isAuthenticated === false) {
+			handleUpdateReactionCount({
+				variables: {
+					post_id: databaseId,
+					reaction: NcmazFcUserReactionPostActionEnum.View,
+					number: NcmazFcUserReactionPostNumberUpdateEnum.Add_1,
+				},
+			})
+			return
+		}
 
-  const renderHeaderType = () => {
-    const pData = { ...(_post || {}) };
+		// user da dang nhap, va luc nay viewer dang fetch.
+		if (!viewer?.databaseId) {
+			return
+		}
 
-    if (postFormats === "audio") {
-      return <SingleTypeAudio post={pData} />;
-    }
-    if (postFormats === "video") {
-      return <SingleTypeVideo post={pData} />;
-    }
-    if (postFormats === "gallery") {
-      return <SingleTypeGallery post={pData} />;
-    }
+		// khi viewer fetch xong, luc nay viewer da co databaseId, va se update view count voi user la viewer
+		handleUpdateReactionCount({
+			variables: {
+				post_id: databaseId,
+				reaction: NcmazFcUserReactionPostActionEnum.View,
+				number: NcmazFcUserReactionPostNumberUpdateEnum.Add_1,
+				user_id: viewer?.databaseId,
+			},
+		})
+	}, [
+		databaseId,
+		isReady,
+		isAuthenticated,
+		viewer?.databaseId,
+		IS_PREVIEW,
+		isUpdateViewCount,
+	])
 
-    if (ncPostMetaData?.template?.[0] === "style2") {
-      return <DynamicSingleType2 post={pData} />;
-    }
-    if (ncPostMetaData?.template?.[0] === "style3") {
-      return <DynamicSingleType3 post={pData} />;
-    }
-    if (ncPostMetaData?.template?.[0] === "style4") {
-      return <DynamicSingleType4 post={pData} />;
-    }
-    if (ncPostMetaData?.template?.[0] === "style5") {
-      return <DynamicSingleType5 post={pData} />;
-    }
-    return (
-      <SingleType1
-        showRightSidebar={!!ncPostMetaData?.showRightSidebar}
-        post={pData}
-      />
-    );
-  };
+	const renderHeaderType = () => {
+		const pData = { ...(_post || {}) }
 
-  return (
-    <>
-      <PageLayout
-        headerMenuItems={props.data?.primaryMenuItems?.nodes || []}
-        footerMenuItems={props.data?.footerMenuItems?.nodes || []}
-        pageFeaturedImageUrl={featuredImage?.sourceUrl}
-        pageTitle={title}
-        pageDescription={excerpt || ""}
-        generalSettings={
-          props.data?.generalSettings as NcgeneralSettingsFieldsFragmentFragment
-        }
-      >
-        {ncPostMetaData?.showRightSidebar ? (
-          <div>
-            <div className={`relative`}>
-              {renderHeaderType()}
+		if (postFormats === 'audio') {
+			return <SingleTypeAudio post={pData} />
+		}
+		if (postFormats === 'video') {
+			return <SingleTypeVideo post={pData} />
+		}
+		if (postFormats === 'gallery') {
+			return <SingleTypeGallery post={pData} />
+		}
 
-              <div className="container flex flex-col my-10 lg:flex-row ">
-                <div className="w-full lg:w-3/5 xl:w-2/3 xl:pe-20">
-                  <SingleContent post={_post} />
-                </div>
-                <div className="w-full mt-12 lg:mt-0 lg:w-2/5 lg:ps-10 xl:ps-0 xl:w-1/3">
-                  <Sidebar categories={_top10Categories} />
-                </div>
-              </div>
+		if (ncPostMetaData?.template?.[0] === 'style2') {
+			return <DynamicSingleType2 post={pData} />
+		}
+		if (ncPostMetaData?.template?.[0] === 'style3') {
+			return <DynamicSingleType3 post={pData} />
+		}
+		if (ncPostMetaData?.template?.[0] === 'style4') {
+			return <DynamicSingleType4 post={pData} />
+		}
+		if (ncPostMetaData?.template?.[0] === 'style5') {
+			return <DynamicSingleType5 post={pData} />
+		}
+		return (
+			<SingleType1
+				showRightSidebar={!!ncPostMetaData?.showRightSidebar}
+				post={pData}
+			/>
+		)
+	}
 
-              {/* RELATED POSTS */}
-              <DynamicSingleRelatedPosts
-                posts={_relatedPosts}
-                postDatabaseId={databaseId}
-              />
-            </div>
-          </div>
-        ) : (
-          <div>
-            {renderHeaderType()}
+	return (
+		<>
+			<PageLayout
+				headerMenuItems={props.data?.primaryMenuItems?.nodes || []}
+				footerMenuItems={props.data?.footerMenuItems?.nodes || []}
+				pageFeaturedImageUrl={featuredImage?.sourceUrl}
+				pageTitle={title}
+				pageDescription={excerpt || ''}
+				generalSettings={
+					props.data?.generalSettings as NcgeneralSettingsFieldsFragmentFragment
+				}
+			>
+				{ncPostMetaData?.showRightSidebar ? (
+					<div>
+						<div className={`relative`}>
+							{renderHeaderType()}
 
-            <div className="container mt-10">
-              {/* SINGLE MAIN CONTENT */}
-              <SingleContent post={_post} />
-            </div>
+							<div className="container my-10 flex flex-col lg:flex-row">
+								<div className="w-full lg:w-3/5 xl:w-2/3 xl:pe-20">
+									<SingleContent post={_post} />
+								</div>
+								<div className="mt-12 w-full lg:mt-0 lg:w-2/5 lg:ps-10 xl:w-1/3 xl:ps-0">
+									<Sidebar categories={_top10Categories} />
+								</div>
+							</div>
 
-            {/* RELATED POSTS */}
-            <DynamicSingleRelatedPosts
-              posts={_relatedPosts}
-              postDatabaseId={databaseId}
-            />
-          </div>
-        )}
-      </PageLayout>
-    </>
-  );
-};
+							{/* RELATED POSTS */}
+							<DynamicSingleRelatedPosts
+								posts={_relatedPosts}
+								postDatabaseId={databaseId}
+							/>
+						</div>
+					</div>
+				) : (
+					<div>
+						{renderHeaderType()}
+
+						<div className="container mt-10">
+							{/* SINGLE MAIN CONTENT */}
+							<SingleContent post={_post} />
+						</div>
+
+						{/* RELATED POSTS */}
+						<DynamicSingleRelatedPosts
+							posts={_relatedPosts}
+							postDatabaseId={databaseId}
+						/>
+					</div>
+				)}
+			</PageLayout>
+		</>
+	)
+}
 
 Component.variables = ({ databaseId }, ctx) => {
-  return {
-    databaseId,
-    post_databaseId: Number(databaseId || 0),
-    asPreview: ctx?.asPreview,
-    headerLocation: PRIMARY_LOCATION,
-    footerLocation: FOOTER_LOCATION,
-  };
-};
+	return {
+		databaseId,
+		post_databaseId: Number(databaseId || 0),
+		asPreview: ctx?.asPreview,
+		headerLocation: PRIMARY_LOCATION,
+		footerLocation: FOOTER_LOCATION,
+	}
+}
 
 Component.query = gql(`
   query GetPostSiglePage($databaseId: ID!, $post_databaseId: Int,$asPreview: Boolean = false, $headerLocation: MenuLocationEnum!, $footerLocation: MenuLocationEnum!) {
     post(id: $databaseId, idType: DATABASE_ID, asPreview: $asPreview) {
-    ...NcmazFcPostFullFields
+		...NcmazFcPostFullVsEditorBlocksNoContentFields
     }
     posts(where: {isRelatedOfPostId:$post_databaseId}) {
       nodes {
@@ -267,6 +278,6 @@ Component.query = gql(`
       }
     }
   }
-`);
+`)
 
-export default Component;
+export default Component
